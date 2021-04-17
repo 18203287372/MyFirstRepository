a2a61ad14ee52a9f93099522c9bf6cf50dabe7e6
zby <1907277862@qq.com>
2021-04-17 15:40:16 +0800

    --[M] 修改pomjj

--- src/main/java/com/lening/controller/MeunController.java
+++ src/main/java/com/lening/controller/MeunController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/meun")
 public class MeunController {
 
+
+
     @Autowired
     private MeunService meunService;
 
